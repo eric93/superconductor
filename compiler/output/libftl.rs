6a0b1b9a7639d56a00586de358f3c200314f8b13
Eric Atkinson <ericatkinson@berkeley.edu>
2014-02-11 13:55:18 -0800

    Initial copy from HTML5 backend & basic functionality

---
+++ compiler/output/libftl.rs
@@ -0,0 +1,75 @@
+use std::hashmap::HashMap;
+use std::util;
+
+
+pub enum NodeType {
+    midnode,
+    leaf,
+}
+
+pub struct FtlNode<'a>{
+    id : int,
+    ty: NodeType,
+    attribs: HashMap<&'a str,int>,
+    kids: ~[FtlNode<'a>],
+}
+
+impl<'a> FtlNode<'a> {
+    pub fn setAttrib(&mut self, attr: &'a str, value: int) {
+        self.attribs.insert_or_update_with(attr, value, |_,v| { *v = value });
+    }
+    pub fn getAttrib(&self, attr: &'a str) -> int {
+        match self.attribs.find_copy(&attr) {
+            Some(a) => a,
+            _ => fail!("Attribute not found: " + attr)
+        }
+    }
+    pub fn with_kids(&mut self, func: |&mut FtlNode<'a>, &mut FtlNode<'a>|) {
+        let mut kids = util::replace(&mut self.kids, ~[]);
+        for child in kids.mut_iter() {
+            func(self, child);
+        }
+        util::replace(&mut self.kids, kids);
+    }
+}
+
+macro_rules! setAttr(
+    ($n:ident,$attr:expr,$v:expr) => (let x = $v; $n.setAttrib($attr,x);)
+)
+
+pub fn log(logstr: &str){
+    println!("{:s}",logstr)
+}
+
+pub fn createNode<'a>(id: int, ty: NodeType, children: ~[FtlNode<'a>]) -> FtlNode<'a> {
+    let mut node = FtlNode{
+        id: id,
+        ty: ty,
+        attribs: HashMap::new(),
+        kids: children
+    };
+
+    node.attribs.insert("numin",id);
+    node
+}
+
+pub fn generateTree<'a>() -> FtlNode<'a> {
+    let leafs1 = ~[createNode(1,leaf,~[]),createNode(2,leaf,~[]),createNode(3,leaf,~[])];
+    let leafs2 = ~[createNode(4,leaf,~[]),createNode(5,leaf,~[])];
+    let parents = ~[createNode(6,midnode,leafs1),createNode(7,midnode,leafs2)];
+    createNode(8,midnode,parents)
+}
+
+pub fn inherit(visit: |&mut FtlNode|,node: &mut FtlNode) {
+    visit(node);
+    for child in node.kids.mut_iter() {
+        inherit(|node| visit(node),child);
+    }
+}
+
+pub fn synthesize(visit: |&mut FtlNode|, node: &mut FtlNode) {
+    for child in node.kids.mut_iter() {
+        synthesize(|node| visit(node),child);
+    }
+    visit(node);
+}
